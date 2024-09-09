3572c8874c63b61a1a634b88429dd696a07f5df8
freddy.dev <omoyeleomojole2004@gmail.com>
2024-09-09 17:05:12 +0100

    controlled state for the more route

--- app/(Home)/components/Header.tsx
+++ app/(Home)/components/Header.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import Navbar from "./Navbar";
 const Header = () => {
-  const [active, setActive] = useState(false);
+  // const [active, setActive] = useState(false);
   const [show, setShow] = useState(false);
 
   interface MoreListProps {
@@ -24,7 +24,7 @@ const Header = () => {
       name: "Soundz for the Classroom",
     },
     {
-      name: "Soundz for the Colleges and Universities",
+      name: "Soundz for  Colleges and Universities",
     },
     {
       name: "Certified Training",
@@ -83,47 +83,55 @@ const Header = () => {
             />
           </div>
           <Navbar
+            onShow={show}
+            setShow={setShow}
             containerStyle="flex items-center gap-8 "
-            linkStyle="capitalize font-semibold text-lg"
+            linkStyle="capitalize font-semibold text-lg cursor-pointer"
           />
         </div>
       </div>
-      <div className="container ml-6 m-4">
-        <h1 className="text-2xl font-semibold"> More on Soundz.com: </h1>
-        <div className="flex mt-4 items-center gap-3">
-          {moreLists.map((list, index) => {
-            return (
-              <div className="" key={index}>
-                {index === moreLists.length - 3 ? (
-                  <span className="text-orange-400">{list.name}</span>
-                ) : (
-                  list.name
-                )}
+
+      <div onClick={() => setShow(!show)}>
+        {show && (
+          <>
+            {" "}
+            <div className="container ml-6 m-4">
+              <h1 className="text-2xl font-semibold"> More on Soundz.com: </h1>
+              <div className="flex mt-4 items-center gap-6">
+                {moreLists.map((list, index) => {
+                  return (
+                    <div className="cursor-pointer" key={index}>
+                      {index === moreLists.length - 3 ? (
+                        <span className="text-orange-400 ">{list.name}</span>
+                      ) : (
+                        list.name
+                      )}
+                    </div>
+                  );
+                })}
               </div>
-            );
-          })}
-        </div>
-      </div>
-      <h1 className=" ml-6 m-4 text-2xl mt-6 font-semibold">
-        {" "}
-        More from Soundz:{" "}
-      </h1>
-      <div className="ml-6 m-4 mb-16 gap-8 grid grid-cols-4">
-        {aboutLists.map((list, index) => {
-          return (
-            <>
-              <div key={index} className="capitalize">
-                <div className="mb-4">
-                  <h1 className="font-semibold">{list.name}</h1>
-                </div>
-                {/* <div> */}
-                  {" "}
-                  <p>{list.label}</p>
-                {/* </div> */}
-              </div>
-            </>
-          );
-        })}{" "}
+            </div>
+            <h1 className=" ml-6 m-4 text-2xl mt-6 font-semibold">
+              {" "}
+              More from Soundz:{" "}
+            </h1>
+            <div className="ml-6 m-4 mb-16 gap-8 grid grid-cols-4">
+              {aboutLists.map((list, index) => {
+                return (
+                  <>
+                    <div key={index} className="capitalize cursor-pointer">
+                      <div className="mb-2">
+                        <h1 className="font-semibold">{list.name}</h1>
+                      </div>
+                      {/* <div> */} <p>{list.label}</p>
+                      {/* </div> */}
+                    </div>
+                  </>
+                );
+              })}{" "}
+            </div>
+          </>
+        )}
       </div>
     </header>
   );
--- app/(Home)/components/Navbar.tsx
+++ app/(Home)/components/Navbar.tsx
@@ -1,38 +1,48 @@
 import React from "react";
-import { FaPlus } from "react-icons/fa";
+import { FaMinus, FaPlus } from "react-icons/fa";
 interface navRouteProps {
   name: string;
   icon?: JSX.Element;
 }
 
-interface StyleProps {
+interface Props {
   containerStyle: string;
   linkStyle: string;
+  onShow: boolean;
+  setShow: (onShow: boolean) => void;
 }
 
-function Navbar({ containerStyle, linkStyle }: StyleProps) {
+function Navbar({ containerStyle, linkStyle, onShow, setShow }: Props) {
+  
+
   const navRoutes: navRouteProps[] = [
-    { name: "live" },
+    { name: "live"},
     { name: " push" },
     { name: "note" },
     { name: "link" },
     { name: "shop" },
     { name: "packs" },
     { name: "help" },
-    { name: "more", icon: <FaPlus /> },
+    { name: "more", icon: onShow ? <FaMinus /> : <FaPlus /> },
   ];
+  const toggleShowBtn = () => {
+    setShow(!onShow);
+  };
 
   return (
     <section>
       <nav className={`${containerStyle}`}>
-        {" "}
+        
         <>
           {" "}
           {navRoutes.map((route, index) => {
             return (
               <div className={`${linkStyle}`} key={index}>
                 {index === navRoutes.length - 1 ? (
-                  <span className="flex items-center gap-2 text-orange-400">
+                  <span
+                    onClick={toggleShowBtn}
+                    className="flex items-center gap-2 text-orange-400"
+                  >
                     {route.name}
                     {route.icon}
                   </span>
@@ -44,17 +54,15 @@ function Navbar({ containerStyle, linkStyle }: StyleProps) {
           })}{" "}
           <div className="flex flex-grow w-96 mx-6 py-3"></div>
           {/* <div className="flex items-center gap-6"> */}
-          <ul className="flex items-center gap-6">
-            <li className="text-blue-600 font-semibold">Try Live 12 for free</li>
+          <ul className="flex items-center gap-6 cursor-pointer">
+            <li className="text-blue-600 font-semibold">
+              Try Live 12 for free
+            </li>
             <li className="font-medium text-sm">log in or register</li>
           </ul>
-          {/* </div> */}
+        
         </>
       </nav>
-
-
-
-
     </section>
   );
 }
