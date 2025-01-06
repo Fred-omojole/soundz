a48e77a9d82b152971c1bf356fdb594e7fd377b9
freddy.dev <omoyeleomojole2004@gmail.com>
2025-01-06 15:07:15 +0100

    seo optimization

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,14 +17,41 @@ const poppins = Poppins({
 //   variable: "--font-geist-mono",
 //   weight: "100 900",
 // });
-
 export const metadata: Metadata = {
-  title: "Soundz by Freddy.dev",
-  description: "website for music tech",
+  title: "Soundz by Freddy.dev | Music Tech Platform",
+  description:
+    "Explore Soundz, a cutting-edge platform by Freddy.dev, dedicated to revolutionizing music technology and enhancing your audio experience.",
   icons: {
     icon: ["/logot.png?v=4"],
     shortcut: ["/logot.png"],
+    apple: ["/logot.png"],
   },
+  openGraph: {
+    title: "Soundz by Freddy.dev | Music Tech Platform",
+    description:
+      "Discover Soundz by Freddy.dev, the ultimate music tech platform delivering innovation and seamless audio solutions.",
+    url: "https://soundz-five.vercel.app",
+    siteName: "Soundz by Freddy.dev",
+    images: [
+      {
+        url: "/imgaes/william.png", // (recommended dimensions: 1200x630)
+        width: 1200,
+        height: 630,
+        alt: "Soundz by Freddy.dev Open Graph Image",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image", // Optimized card type for Twitter
+    title: "Soundz by Freddy.dev | Music Tech Platform",
+    description:
+      "Revolutionizing music technology with Soundz, your go-to platform for modern audio innovation.",
+    images: ["/images/william.png"], // Ensure this matches the Open Graph image
+  },
+  viewport: "width=device-width, initial-scale=1", // Essential for responsive design
+  themeColor: "#000000", // Sets the browser theme color for mobile
 };
 
 export default function RootLayout({
